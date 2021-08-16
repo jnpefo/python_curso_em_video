ea1024a0038e19bf4589fd495abe4d41c19db1cd
jnpefo <jnpefo@gmail.com>
2021-08-16 00:36:13 -0300

    Primeiro dia

---
+++ aula4/desafio.py
@@ -0,0 +1,19 @@
+#Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+print('===== DESAFIO 01 =====')
+nome = input('Qual é o seu nome? ')
+print('Olá ', nome, '! Prazer em te conhecer!')
+
+#Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+print('===== DESAFIO 02 =====')
+dia = input('Qual dia em que você nasceu? ')
+mes = input('Qual mês em que você nasceu? ')
+ano = input('Qual ano em que você nasceu? ')
+print('Você nasceu no dia ', dia, 'de ', mes, 'de ', ano, '. Correto?')
+
+#Crei um script Python que leia dois números e tente mostrar a soma entre eles.
+
+print('===== DESAFIO 03 =====')
+num1 = input('Primeiro número: ')
+num2 = input('Segundo número: ')
+soma = int(num1) + int(num2)
+print('A soma é ', soma)
